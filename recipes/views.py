79e97fcc95c33093959a89ef19c1c0787742c772
milbratheduardo <milbratheduardo@gmail.com>
2022-04-25 20:44:02 -0300

    dados_receitas

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes' : [make_recipe() for _ in range(6)],
+    })
 
 def recipes(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe' : make_recipe(),
+    })
 
 
 
