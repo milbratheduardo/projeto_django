62613cf7499a8437f594c554290195401b1aeef7
milbratheduardo <milbratheduardo@gmail.com>
2022-04-27 12:49:54 -0300

    queryset

--- recipes/models.py
+++ recipes/models.py
@@ -10,6 +10,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Recipe(models.Model):
   title = models.CharField(max_length=65) 
   description = models.CharField(max_length=165) 
@@ -27,3 +28,6 @@ class Recipe(models.Model):
   category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
   author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
+
+  def __str__(self):
+      return self.title
