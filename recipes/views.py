e8f5f8de4e443d3400473528296a86d95a446468
milbratheduardo <milbratheduardo@gmail.com>
2022-04-28 15:32:53 -0300

    404

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,6 @@
+from turtle import title
+
+from django.http import Http404
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
@@ -11,12 +14,20 @@ def home(request):
     })
 
 
+
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id = category_id,
     is_published = True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not found 🥲')
+        
     return render(request, 'recipes/pages/category.html', context={
         'recipes' : recipes,
+        'title': f'{recipes.first().category.name} - Category | '
     })
+
+    
 
 def recipes(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
